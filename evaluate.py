b4028e2ce10790d427f77c78058e86e2499f4aad
Jacob Allen Utley <jacob@halfinjest.com>
2017-10-28 16:52:35 -0500

    Updated in response to changes.

--- evaluate.py
+++ evaluate.py
@@ -1,17 +1,17 @@
+import requests
 import threading
-import urllib2
 from attempt import *
 
 def evaluate(number, passwords):
 	try:
 		entry = [number]
-		request = urllib2.Request("http://www.zerohedge.com/user/" + \
-			str(number))
-		response = urllib2.urlopen(request).read()
-		entry.append(response[response.index("canonical") + \
-			48:response.index("revisit-after") - 17])
-		entry.append(response[response.index("<title>") + \
-			7:response.index(" | Zero Hedge")])
-		threading.Thread(target=attempt,
-			args=(entry, passwords)).start()
+		response = requests.get("http://zerohedge.com/user/" + \
+			str(number)).text
+		if "Page Not Found" not in response:
+			entry.append(response[response.index("canonical") + \
+				48:response.index("og:url") - 21])
+			entry.append(response[response.index("<title>") + \
+				7:response.index(" | Zero Hedge")])
+			threading.Thread(target=attempt,
+				args=(entry, passwords)).start()
 	except Exception as error: pass
